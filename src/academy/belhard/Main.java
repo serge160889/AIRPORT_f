77fb760c79d1e341ce71c78a2cc49b5cf4f06f31
serge160889 <serge_voit_2006@mail.ru>
2021-04-12 23:21:43 +0300

    no exceptions(1)

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -18,25 +18,25 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-//        PilotDataReader pilotDataReader = new PilotDataReader("C:\\Users\\serge\\IdeaProjects\\AIRPORT_f\\db\\data\\Pilots.csv");
-//        List<Pilot> pilots = pilotDataReader.read();
-//        pilots.forEach(System.out::println);
-//        System.out.println();
-//        System.out.println();
-//        AirplaneDataReader airplaneDataReader = new AirplaneDataReader("C:\\Users\\serge\\IdeaProjects\\AIRPORT_f\\db\\data\\Airplanes.csv");
-//        List<Airplane> airplanes = airplaneDataReader.read();
-//        airplanes.forEach(System.out::println);
-//        System.out.println();
-//        System.out.println();
-//
-//        FlightDataReader flightDataReader = new FlightDataReader("C:\\Users\\serge\\IdeaProjects\\AIRPORT_f\\db\\data\\Flights.csv");
-//        List<Flight> flights = flightDataReader.read();
-//        flights.forEach(System.out::println);
-//
-//
-//        PilotDbWriter.writePilots(pilots);
-//        AirplaneDbWriter.writeAirplane(airplanes);
-//        FlightDbWriter.writeFlight(flights);
+        PilotDataReader pilotDataReader = new PilotDataReader("C:\\Users\\serge\\IdeaProjects\\AIRPORT_f\\db\\data\\Pilots.csv");
+        List<Pilot> pilots = pilotDataReader.read();
+        pilots.forEach(System.out::println);
+        System.out.println();
+        System.out.println();
+        AirplaneDataReader airplaneDataReader = new AirplaneDataReader("C:\\Users\\serge\\IdeaProjects\\AIRPORT_f\\db\\data\\Airplanes.csv");
+        List<Airplane> airplanes = airplaneDataReader.read();
+        airplanes.forEach(System.out::println);
+        System.out.println();
+        System.out.println();
+
+        FlightDataReader flightDataReader = new FlightDataReader("C:\\Users\\serge\\IdeaProjects\\AIRPORT_f\\db\\data\\Flights.csv");
+        List<Flight> flights = flightDataReader.read();
+        flights.forEach(System.out::println);
+
+
+        PilotDbWriter.writePilots(pilots);
+        AirplaneDbWriter.writeAirplane(airplanes);
+        FlightDbWriter.writeFlight(flights);
 
         FullDataReader.read().forEach(System.out::println);
         System.out.println();
